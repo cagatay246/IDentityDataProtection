6d3c6b43c165c40e4a526e3f297af334bcde26d4
cagatay246 <cagataykilgi7@gmail.com>
2025-04-17 14:42:12 +0300

    2

---
+++ IdentityAndDataProtection/Context/IdentityAndDataProtectionDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using IdentityAndDataProtection.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAndDataProtection.Context;
+
+public class IdentityAndDataProtectionDbContext : IdentityDbContext<IdentityUser>
+{
+    public IdentityAndDataProtectionDbContext(DbContextOptions<IdentityAndDataProtectionDbContext> options) :
+        base(options)
+    {
+    }
+    public DbSet<UserEntity> User { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# IdentityAndDataProtection
+
+
+## User Management and Database Creation
+
+- A User table was created and Email and Password fields were defined in this table.
+
+- Then, a database was created using the Entity Framework Code First approach and user records were added to this table.
+
+- Model Validation was done for the API. ( [Required] )
+
+- The Password field was encrypted during the registration process.
